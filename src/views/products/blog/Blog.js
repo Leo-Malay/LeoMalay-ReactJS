74c5a7fe7f93e8f4bbda2c788cb74b4af6893ebc
LEO-Malay <malaybhavsar.290@gmail.com>
2021-11-04 17:30:08 +0530

    Added Filter in Blog. Created Blog Write

--- src/views/products/blog/Blog.js
+++ src/views/products/blog/Blog.js
@@ -2,14 +2,59 @@ import Header from "../../../components/Header";
 import BlogCard from "./components/BlogCard";
 import "./css/Blog.css";
 import BlogData from "../../../data/Blog.json";
+import { useState } from "react";
 const Blog = () => {
+    const [filter, setFilter] = useState("");
+    const renderData = () => {
+        if (filter === "") return BlogData;
+        return BlogData.filter((BlogData) => {
+            return BlogData.category === filter;
+        });
+    };
     return (
         <div className="Blog">
             <Header
                 props={{ title: "LeoBlog", color: "secondary", type: "Blog" }}
             />
+            <div className="CategoryContainer AlignCenter">
+                <p className="category fs2 primary-nohover">
+                    Filter by Catergory
+                </p>
+                {["Technology", "FrameWork", "Algorithm", "UI/UX"].map(
+                    (ele, i) => {
+                        return (
+                            <p
+                                className="category fs2 secondary-nohover"
+                                key={i}
+                                nodevalue={ele}
+                                onClick={(e) =>
+                                    setFilter(e.target.childNodes[0].data)
+                                }
+                            >
+                                {ele.toUpperCase()}
+                            </p>
+                        );
+                    }
+                )}
+                {filter !== "" && (
+                    <p
+                        className="category fs2 error-nohover"
+                        onClick={(e) => setFilter("")}
+                    >
+                        Clear Filter
+                    </p>
+                )}
+            </div>
             <div className="BlogContainer">
-                {BlogData.map((ele, i) => {
+                {filter !== "" && (
+                    <div className="FilterHeading AlignCenter">
+                        <p className="category fs10 secondary-nohover bold">
+                            {filter}
+                        </p>
+                    </div>
+                )}
+                <br />
+                {renderData().map((ele, i) => {
                     return (
                         <BlogCard
                             key={i}
--- src/views/products/blog/BlogWrite.js
+++ src/views/products/blog/BlogWrite.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import Header from "../../../components/Header";
 const BlogWrite = () => {
+    const [title, setTitle] = useState("");
+    const [thumbnail, setThumbnail] = useState("");
+    const [author, setAuthor] = useState("");
+    const [category, setCategory] = useState("");
+    const [description, setDescription] = useState("");
     const submitHandler = (e) => {
         e.preventDefault();
+        const data = { title, thumbnail, author, category, description };
+        console.log(data);
     };
     return (
         <div className="BlogWrite">
@@ -18,16 +26,68 @@ const BlogWrite = () => {
                     type="text"
                     name="title"
                     placeholder="Title of Blog"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                     required
                     style={{ width: "80%" }}
                 />
                 <br />
+                <input
+                    type="url"
+                    name="thumbnail"
+                    value={thumbnail}
+                    onChange={(e) => setThumbnail(e.target.value)}
+                    placeholder="URL of Thumbnail Image"
+                    required
+                    style={{ width: "80%" }}
+                />
+                <br />
+                <input
+                    type="text"
+                    name="author"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                    placeholder="Name of Author"
+                    required
+                    style={{ width: "80%" }}
+                />
+                <select
+                    name="category"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    required
+                    style={{ width: "80%" }}
+                >
+                    <option value="" disabled selected>
+                        Choose an Category
+                    </option>
+                    {["Algorithm", "UI/UX", "Technology", "Frameworks"].map(
+                        (ele, i) => {
+                            return (
+                                <option value={ele} key={i}>
+                                    {ele}
+                                </option>
+                            );
+                        }
+                    )}
+                </select>
+                <br />
                 <br />
                 <textarea
                     rows={20}
-                    placeholder="Start Typing Here..."
+                    name="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    placeholder="Start Typing Here... #newPara$ ---> For New Paragraph"
+                    minLength={200}
+                    required
                     style={{ width: "80%", padding: 10, borderRadius: 5 }}
                 />
+                <p className="fs2 bold">
+                    Length: {description.replaceAll(" ", "").length}, Words:{" "}
+                    {description.split(" ").length}
+                </p>
+                <br />
                 <input
                     type="submit"
                     name="Submit"
@@ -35,6 +95,19 @@ const BlogWrite = () => {
                     style={{ width: "40%" }}
                 />
             </form>
+            <br />
+            <br />
+            <p className="AlignCenter fs17 bold">Preview</p>
+            <br />
+            {description.split("#newPara$").map((ele, i) => {
+                return (
+                    <p className="AlignJustify fs4 preview">
+                        {ele}
+                        <br />
+                        <br />
+                    </p>
+                );
+            })}
         </div>
     );
 };
