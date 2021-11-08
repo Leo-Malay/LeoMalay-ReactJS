1750568c73c6e5541653a0004b9fd400511f9145
LEO-Malay <malaybhavsar.290@gmail.com>
2021-11-08 20:57:24 +0530

    Created Handler for Like, Comment and Share

--- src/views/products/social/Home.js
+++ src/views/products/social/Home.js
@@ -34,6 +34,42 @@ const Home = () => {
                         ],
                     }}
                 />
+                <SocialPost
+                    props={{
+                        avatar: true,
+                        id: 1,
+                        fname: "Malay",
+                        lname: "Bhavsar",
+                        postImage: true,
+                        postText: true,
+                        postTextData: "Hello World",
+                        comments: [],
+                    }}
+                />
+                <SocialPost
+                    props={{
+                        avatar: true,
+                        id: 2,
+                        fname: "Malay",
+                        lname: "Bhavsar",
+                        postImage: false,
+                        postText: true,
+                        postTextData:
+                            "Hello World, This is my very first post on LeoSocial which is developd by Malay Bhavsar and is the future of the earth.",
+                        comments: [
+                            {
+                                fname: "Harry",
+                                lname: "Potter",
+                                body: "Nice Pic",
+                            },
+                            {
+                                fname: "John",
+                                lname: "Wick",
+                                body: "Wanna be in my movie?",
+                            },
+                        ],
+                    }}
+                />
             </div>
         </div>
     );
--- src/views/products/social/components/Post.js
+++ src/views/products/social/components/Post.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import imagePost from "../../../../assets/iphone.jfif";
 import "../css/Post.css";
 const SocialPost = ({ props }) => {
+    const [newComment, setNewComment] = useState("");
+    const [showCommentInput, setShowCommentInput] = useState(false);
+    const likeHandler = (e) => {
+        e.preventDefault();
+        console.log("Liked the Post");
+    };
+    const commentHandler = (e) => {
+        e.preventDefault();
+        setShowCommentInput(!showCommentInput);
+    };
+    const shareHandler = (e) => {
+        e.preventDefault();
+        console.log("Shared the Post");
+    };
+    const addCommentHandler = (e) => {
+        props.comments.push({
+            fname: "Malay",
+            lname: "Bhavsar",
+            body: newComment,
+        });
+        setNewComment("");
+        setShowCommentInput(false);
+        console.log("Added new Comment to the Post");
+    };
     return (
         <div className="SocialPost">
             <div className="head AlignLeft">
@@ -18,17 +43,43 @@ const SocialPost = ({ props }) => {
                 {props.postImage && (
                     <img src={props.postImage && imagePost} alt="Post Pic" />
                 )}
-                {props.postText && <p>{props.postTextData}</p>}
+                {props.postText && (
+                    <p className="fs4 text AlignJustify">
+                        {props.postTextData}
+                    </p>
+                )}
             </div>
             <div className="foot">
                 <div className="ButtonContainer">
-                    <button className="fs3 bold">Like</button>
-                    <button className="fs3 bold">Comment</button>
-                    <button className="fs3 bold">Share</button>
+                    <button className="fs3 bold" onClick={likeHandler}>
+                        Like
+                    </button>
+                    <button className="fs3 bold" onClick={commentHandler}>
+                        Comment
+                    </button>
+                    <button className="fs3 bold" onClick={shareHandler}>
+                        Share
+                    </button>
                 </div>
                 <div className="LikeContainer">
                     <p className="fs2 AlignLeft bold">Liked by 29M People</p>
                 </div>
+                {showCommentInput && (
+                    <div className="NewCommentContainer">
+                        <input
+                            type="text"
+                            placeholder="Write Comment Here..."
+                            value={newComment}
+                            onChange={(e) => setNewComment(e.target.value)}
+                        />
+                        <input
+                            type="submit"
+                            value="Add"
+                            className="success-nohover"
+                            onClick={addCommentHandler}
+                        />
+                    </div>
+                )}
                 <div className="CommentContainer AlignCenter">
                     {props.comments.map((ele, i) => {
                         return (
